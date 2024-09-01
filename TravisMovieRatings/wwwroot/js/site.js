a9a35eb9db0c026b6f4c3b4759d106fb121000d5
Travis Dutchover <travis.dutchover@gmail.com>
2024-09-01 03:52:03 -0700

    Refine user interface for better user experience and make corresponding updates to README.md

--- TravisMovieRatings/wwwroot/js/site.js
+++ TravisMovieRatings/wwwroot/js/site.js
@@ -5,16 +5,3 @@ function isNewTab() {
     const SINGLE_PAGE_HISTORY_LENGTH = 1;
     return window.history.length <= SINGLE_PAGE_HISTORY_LENGTH;
 }
-
-function goBackToPreviousPage() {
-    if (isNewTab()) {
-        // Redirect to the home page if there's no previous page in the history.
-        // This catch-all case can happen when the user opens the page in a new browser tab or window.
-        window.location.href = "/";
-    } else {
-        // This is the typical case where the user navigated to the page from another page.
-        window.history.back();
-    }
-}
-
-
